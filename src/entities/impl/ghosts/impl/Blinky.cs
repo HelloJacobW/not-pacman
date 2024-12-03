d60b0f857324b029c16ac157fc06ee77812a5663
Dylan Bruner <dybr78f@student.oaisd.org>
2024-12-03 14:36:31 -0500

    not done but ryan needs

--- src/entities/impl/ghosts/impl/Blinky.cs
+++ src/entities/impl/ghosts/impl/Blinky.cs
@@ -1,16 +1,25 @@
-﻿using System.Windows.Media;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Documents;
+using System.Windows.Media;
+using WpfApp1.src.entities.impl.ghosts.impl.helpers;
 
 namespace WpfApp1.src.entities.impl.ghosts
 {
     class Blinky : Ghost
     {
+        private List<PathfindingSystem.ResultPoint> path = null;
+        private DateTime lastMoveTime = DateTime.MinValue;
+
         public Blinky() {
             color = Brushes.Red;
         }
 
         public override void update(double deltaTime)
         {
-            // movement logic
+            if (path ==  null)
+            {
+            }
         }
     }
 }
