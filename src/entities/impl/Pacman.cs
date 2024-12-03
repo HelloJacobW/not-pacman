96446ac54c5206ac9a50103566ca5a43717b115d
Ryan-Haug-Student <ryha601@student.oaisd.org>
2024-12-03 14:36:55 -0500

    temp save

--- src/entities/impl/Pacman.cs
+++ src/entities/impl/Pacman.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Diagnostics;
+using System.Numerics;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using WpfApp1;
+using WpfApp1.src;
+using WpfApp1.src.helpers;
 
 public class Pacman : Entity
 {
-    public bool moveRight = true;
-    public bool moveLeft;
-    public bool moveUp;
-    public bool moveDown;
+    private MoveDirection moveDirection = MoveDirection.NONE;
+
+    public DateTime elapsedTime;
 
     public Pacman()
     {
@@ -16,35 +20,78 @@ public class Pacman : Entity
         SpriteShape.Fill = Brushes.Yellow;
         SpriteShape.Width = 20;
         SpriteShape.Height = 20;
+
+        position = new System.Numerics.Vector2(25, 25);
+        elapsedTime = DateTime.Now;
     }
 
     public override void update(double deltaTime)
     {
-     if (moveRight && position.X < 700)
+     if((DateTime.Now - elapsedTime).TotalMilliseconds >= 250)
         {
-            position.X += (float)(100f * deltaTime);
-        }
-     if (moveLeft && position.X > 0)
-        {
-            position.X -= (float)(100f * deltaTime);
-        }
+            if (moveDirection == MoveDirection.RIGHT && )
+            {
+                position.X += (float)(20f);
+            }
+            if (moveDirection == MoveDirection.LEFT && position.X > 25)
+            {
+                position.X -= (float)(20f);
+            }
 
-     if (moveUp && position.Y <= 0)
-        {
-            position.Y -= (float)(100f * deltaTime);
-        }
-     if (moveDown && position.Y < 540)
-        {
-            position.Y += (float)(100f * deltaTime);
+            if (moveDirection == MoveDirection.UP && position.Y > 25)
+            {
+                position.Y -= (float)(20f);
+            }
+            if (moveDirection == MoveDirection.DOWN && position.Y < 540)
+            {
+                position.Y += (float)(20f);
+            }
+
+            elapsedTime = DateTime.Now;
         }
     }
 
-
     public void OnKeyDown(KeyEventArgs e)
     {
+        if (e.Key == Key.W)
+        {
+            moveDirection = MoveDirection.UP;
+        }
+        if (e.Key == Key.S)
+        {
+            moveDirection = MoveDirection.DOWN;
+        }
+
+        if (e.Key == Key.D)
+        {
+            moveDirection = MoveDirection.RIGHT;
+        }
+        if(e.Key == Key.A)
+        {
+            moveDirection = MoveDirection.LEFT;
+        }
     }
 
     public void OnKeyUp(KeyEventArgs e)
     {
+    }
+
+    private bool DoesIntersect(Vector2 pos)
+    {
+        Vector2 playerMapPos = Vector2.Zero;
+        var playerRect = new WpfApp1.src.helpers.Rectangle(playerMapPos.X, playerMapPos.Y, 1, 1);
+        
+        foreach (var wall in Constants.walls)
+        {
+            if (wall.Intersects(playerRect))
+                return true;
+        }
+
+        return false;
+    }
+
+    private enum MoveDirection
+    {
+        LEFT, RIGHT, UP, DOWN, NONE
     }
 }
