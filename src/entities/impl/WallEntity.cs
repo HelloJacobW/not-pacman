493dac1813fdbb1dd5d2115efb76a8ff4e655abb
Ryan-Haug-Student <ryha601@student.oaisd.org>
2024-12-09 11:57:28 -0500

    deleted non working wall code

--- src/entities/impl/WallEntity.cs
+++ src/entities/impl/WallEntity.cs
@@ -12,8 +12,6 @@ namespace WpfApp1.src.entities.impl
 
         public WallEntity(Vector2 position, float width, float heigth)
         {
-            SpriteShape = new CustomShape();
-
             this.position = position;
             SpriteShape.Width = width;
             SpriteShape.Height = heigth;
@@ -23,39 +21,7 @@ namespace WpfApp1.src.entities.impl
 
         public override void update(double deltaTime)
         {
-        }
-
-
-
-        public class CustomShape : Shape
-        {
-            public double Thickness { get; set; } = 5;
-
-            protected override Geometry DefiningGeometry
-            {
-                get
-                {
-                    if (Thickness <= 0 || Width <= Thickness || Height <= Thickness)
-                    {
-                        return Geometry.Empty;
-                    }
-
-                    double halfThickness = Thickness / 2;
-
-                    Rect outerRect = new Rect(0, 0, Width, Height);
-                    Rect innerRect = new Rect(halfThickness, halfThickness, Width - Thickness, Height - Thickness);
-
-                    Geometry outerGeometry = new RectangleGeometry(outerRect);
-                    Geometry innerGeometry = new RectangleGeometry(innerRect);
-
-                    CombinedGeometry hollowedOutGeometry = new CombinedGeometry(GeometryCombineMode.Exclude, outerGeometry, innerGeometry);
-
-                    return hollowedOutGeometry;
-                }
-            }
-
-            public double Width { get; set; } = 100;
-            public double Height { get; set; } = 50;
+        
         }
 
     }
